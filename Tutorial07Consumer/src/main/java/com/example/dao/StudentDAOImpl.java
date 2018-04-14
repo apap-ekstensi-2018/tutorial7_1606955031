1f8cd75584aecd72543c5a39d5cabcd901b7e68f
shafamp <shafamaharani1995@gmail.com>
2018-04-14 12:15:47 +0700

    Updated Lab7

---
+++ Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -0,0 +1,51 @@
+package com.example.dao;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.model.StudentModel;
+
+@Service
+public class StudentDAOImpl implements StudentDAO{
+	
+	@Autowired
+	@Lazy
+	private RestTemplate restTemplate;
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	
+	 @Override
+	 public StudentModel selectStudent (String npm)
+	 {
+		 StudentModel student =
+				 restTemplate.getForObject(
+						 "http://localhost:8080/rest/student/view/"+npm,
+						 StudentModel.class);
+		 return student;
+	 }
+	 
+	
+	
+	@Override
+	public List<StudentModel>selectAllStudents ()
+	{
+		ResponseEntity<StudentModel[]> student = restTemplate.getForEntity("http://localhost:8080/rest/student/viewall", 
+				StudentModel[].class);
+		return Arrays.asList(student.getBody());
+	}
+	
+	
+	
+}
